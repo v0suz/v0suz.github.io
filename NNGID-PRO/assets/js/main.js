5a7dd855d75b0beb993a59da2bcbc43c53e4ab13
v0suz <67117375+v0suz@users.noreply.github.com>
2021-03-21 17:29:25 +0300

    Add files via upload

--- NNGID-PRO/assets/js/main.js
+++ NNGID-PRO/assets/js/main.js
@@ -73,8 +73,56 @@ sr.reveal('.portfolio__img', {interval: 200})
 sr.reveal('.contact__subtitle', {})
 sr.reveal('.contact__text', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
-sr.reveal('.contact__button', {delay: 600})
+sr.reveal('.contact__button', {delay: 600});
 
+var textWrapper = document.querySelector(".intro-title");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /([^\x00-\x80]|\w)/g,
+    "<span class='letter'>$&</span>"
+);
 
+anime
+    .timeline({ loop: false })
+    .add({
+        targets: ".intro-title .letter",
+        translateX: [140, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1400,
+        delay: function(el, i) {
+            return 500 + 50 * i;
+        }
+    })
+    .add({
+        targets: ".intro-title .letter",
+        translateX: [0, -140],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 1200,
+        delay: function(el, i) {
+            return 700 + 50 * i;
+        }
+    });
+
+TweenMax.to(".loader", 2.2, {
+    delay: 5,
+    top: "-100%",
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".logo", 2, {
+    delay: 6,
+    y: 10,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".l-header", 2, {
+    delay: 6,
+    y: 10,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
 
 
