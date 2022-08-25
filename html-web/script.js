60a6e596d96cfef81f5ffb45102164d23107bae3
v0suz <67117375+v0suz@users.noreply.github.com>
2022-08-25 23:47:59 +0300

    Add files via upload

--- html-web/script.js
+++ html-web/script.js
@@ -21,4 +21,112 @@ $(".section.is--hero").each(function (index) {
       y: "-90%",
       duration: 1,
     });
+});
+
+$(".header_text-move").each(function (index) {
+    let triggerElement = $(this);
+    let targetElement = $(this);
+  
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: triggerElement,
+        // trigger element - viewport
+        start: "-100% top",
+        end: "30% top",
+        scrub: 0.8
+      }
+    });
+    tl.to(targetElement, {
+      y: "100%",
+      duration: 1
+    });
   });
+  
+  // Sticky Circle Grow
+  $(".sticky-circle_wrap").each(function (index) {
+    let triggerElement = $(this);
+    let targetElement = $(".sticky-circle_element");
+  
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: triggerElement,
+        // trigger element - viewport
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1
+      }
+    });
+    tl.fromTo(
+      targetElement,
+      {
+        width: "35em",
+        height: "35em",
+        borderRadius: "35em",
+        duration: 1
+      },
+      {
+        width: "100vw",
+        height: "100vh",
+        borderRadius: "0em",
+        duration: 1
+      }
+    );
+  });
+
+  $(".grid_wrapper:nth-child(odd)").each(function (index) {
+    let triggerElement = $(this);
+    let targetElement = $("body, .section.is--nav");
+  
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: triggerElement,
+        // trigger element - viewport
+        // can also use "20px 80%"
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1
+      }
+    });
+    tl.fromTo(
+      targetElement,
+      {
+        backgroundColor: "#e8e2da",
+        color: "#2e2a27",
+        duration: 1
+      },
+      {
+        backgroundColor: "#2e2a27",
+        color: "#e8e2da",
+        duration: 1
+      }
+    );
+  });
+  $(".grid_wrapper:nth-child(even), .sticky-circle_wrap").each(function (index) {
+    let triggerElement = $(this);
+    let targetElement = $("body, .section.is--nav");
+  
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: triggerElement,
+        // trigger element - viewport
+        // can also use "20px 80%"
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1
+      }
+    });
+    tl.fromTo(
+      targetElement,
+      {
+        backgroundColor: "#2e2a27",
+        color: "#e8e2da",
+        duration: 1
+      },
+      {
+        backgroundColor: "#e8e2da",
+        color: "#2e2a27",
+        duration: 1
+      }
+    );
+  });
+  
---
+++ html-web/test.js
@@ -0,0 +1,7 @@
+// const text = document.querySelector('.nav_logo');
+
+// window.addEventListener('scroll', () => {
+//     const current = window.scrollY;
+
+//     text.style.fontSize = `50px`;
+// });
